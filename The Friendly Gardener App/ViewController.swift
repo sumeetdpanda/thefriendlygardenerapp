46defb62b95acc67bf8b2689440b53952c86129b
Sumeet <sumeetshrivas@yahoo.com>
2022-07-26 00:18:56 -0400

    Initial Commit

---
+++ The Friendly Gardener App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Friendly Gardener App
+//
+//  Created by Sumeet Shrivastava on 2022-07-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
